b051c3f5c2d716d6db08c6e66e14ff8732dce9cf
luis <nikko1801@gmail.com>
2020-07-23 20:21:19 -0500

    cahnging aboutme section

--- src/aboutme/AboutMe.jsx
+++ src/aboutme/AboutMe.jsx
@@ -7,8 +7,8 @@ export default class AboutMe extends Component {
         const text = (
             <div className="about">
                 <h1>- Hi! Let me talk (a little) about me</h1>
-                <p>I am a Software developer based in Lima, Perú. I've 5 years of experience in web development, mobile development and software development in general.</p>
-                <p>I enjoy learn about new technologies and I really (really) like doing IA and machine learning in my free time.</p>
+                <p>I am a Software developer based in Lima, Perú. I've 5 years of experience in web development, mobile development, and software development in general.</p>
+                <p>I enjoy learning about new technologies and I really (really) like doing IA and machine learning in my free time.</p>
                 <p>Feel free to contact me for freelance projects, collaborations or say Hi at <a className="mark" href="mailto: luis@autana.com">luis@autana.com</a>.</p>
             </div>
         );
